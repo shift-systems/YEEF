ba6182e7714e919dc9e43cbe94b206fd75603630
kafuuma <arkafuuma@gmail.com>
2019-07-12 16:32:17 +0300

    ch(circleci):intergrate circleci - add .circleci folder to the root project - add config,yml file into the .circleci folder

--- server/api/authentication/models/users.py
+++ server/api/authentication/models/users.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
 )
 from django.db import models
 from django.utils import timezone
 import uuid
 
+ID_LENGTH = 9
+
+
+def id_gen() -> str:
+    """Generates random string whose length is `ID_LENGTH`"""
+    return str(uuid.uuid4())[:ID_LENGTH]
+
 
 class UserManager(BaseUserManager):
-
     def create_user(self, **kwargs):
-        """Create and return a `User` with an email,mobile number, username and password."""
-
+        """
+        Creates and saves a User with the given credentials.
+        """
         email = kwargs.get("email")
         password = kwargs.get("password")
         mobile_number = kwargs.get("mobile_number")
@@ -20,12 +29,12 @@ class UserManager(BaseUserManager):
             self.model.objects.filter(mobile_number=mobile_number).first()
         if user_mobile_number:
             raise ValueError(
-                f"User with mobile number {mobile_number} "
-                "already exists"
+                "User with mobile number {mobile_number} "
+                "already exists".format(mobile_number=mobile_number)
             )
         if user:
             raise ValueError(
-                f"User with email {email} already exists"
+                "User with email {email} already exists".format(email=email)
             )
         user = self.model(
             mobile_number=mobile_number, email=self.normalize_email(email)
@@ -34,61 +43,44 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, username, email, password):
-        """
-          Create and return a `User` with superuser powers.
-          Superuser powers means that this use is an admin that can do anything
-          they want.
-        """
-        if password is None:
-            raise TypeError('Superusers must have a password.')
-
-        user = self.create_user(
-            email=email, username=username, password=password,
-            mobile_number='256788088891')
-        user.is_superuser = True
-        user.is_staff = True
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        user.is_superuser = user.is_staff = True
+        user.is_active = user.is_admin = True
         user.save(using=self._db)
-
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
-    User model
+        User model
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     password = models.CharField(max_length=100)
     mobile_number = models.CharField(max_length=100, null=True, unique=True)
-    username = models.CharField(
-        db_index=True, max_length=255, unique=True, null=True)
-    email = models.EmailField(db_index=True, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
-    deleted_at = models.DateTimeField(blank=True, null=True)
-    deleted_by = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
-
-    """
-    More fields required by Django when specifying a custom user model.
-    The `USERNAME_FIELD` property tells us which field we will use to log in.
-    In this case, we want that to be the email field.
-    """
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    """
-    Tells Django that the UserManager class defined above should manage
-    objects of this type.
-    """
     objects = UserManager()
 
     def __str__(self):
-        """
-        Returns a string representation of this `User`
-        """
         return self.email
+
+    def has_admin_permission(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_permission(self, app_label):
+        return self.is_admin
+
+    def has_super_admin_permission(self, perm, obj=None):
+        return self.is_superuser
 
     def delete(self):
         self.deleted_at = timezone.now()
@@ -96,3 +88,47 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def hard_delete(self):
         super().delete()
+
+
+class Profile(models.Model):
+    """
+        User Role model
+    """
+    profile = models.OneToOneField(
+        "User",
+        null=True, blank=True,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        max_length=50, unique=True
+    )
+    profile_image = models.URLField(
+        default='https://res.cloudinary.com/du3v7gyou/image/upload/v1567601646/yeef/Profile_Picture_Placeholder_qqwprt.png'
+    )
+    job_title = models.CharField(max_length=100, null=True, blank=True)
+    starting_date = models.DateField(blank=True, null=True)
+    birthday = models.DateField(auto_now=False, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Roles"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    """
+        User Role model
+    """
+
+    name = models.CharField(
+        max_length=50, unique=True
+    )
+
+    class Meta:
+        verbose_name_plural = "Roles"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
---
+++ server/utils/uui_util.py
@@ -0,0 +1,9 @@
+import uuid
+from django.utils.http import int_to_base36
+
+ID_LENGTH = 9
+
+
+def id_gen() -> str:
+    """Generates random string whose length is `ID_LENGTH`"""
+    return int_to_base36(uuid.uuid4().int)[:ID_LENGTH]
