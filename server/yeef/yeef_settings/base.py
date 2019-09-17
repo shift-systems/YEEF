c0a57250d1edc6c6fe729a4903aeaaf19c739b6b
kafuuma <arkafuuma@gmail.com>
2019-09-17 17:24:27 +0300

    this is email stuff

--- server/yeef/yeef_settings/base.py
+++ server/yeef/yeef_settings/base.py
@@ -21,6 +21,13 @@ BASE_DIR = os.path.dirname(
         os.path.dirname(os.path.abspath(__file__))
     )
 )
+
+# BASE_DIR = os.path.dirname(
+
+#     os.path.dirname(os.path.abspath(__file__))
+
+# )
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
