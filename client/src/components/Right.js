ec88dddf4e066bb919c1fbbbc9e183f87889ddc7
okellogabrielinnocent@gmail.com <okellogabrielinnocent@gmail.com>
2019-12-05 20:14:33 +0300

    login page

---
+++ client/src/components/Right.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Right = props => {
+    return (
+        <div id="right">
+            <div className="banner">
+                <div className="advert-banner">
+                    <p>Join Us for the best financial savings </p>
+                </div>
+                <a href="" className="button">
+                    <span className="button-Text">Start a FREE 10-day trial</span>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Right;
